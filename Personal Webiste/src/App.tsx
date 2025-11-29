e7df54c6bec6db89c830529942cb2cb8fbaaae37
Ali.Kaan.Koc <alikaansoftdev@gmail.com>
2025-11-29 23:32:23 +0300

    new ScrollToTop.tsx

--- Personal Webiste/src/App.tsx
+++ Personal Webiste/src/App.tsx
@@ -1,13 +1,22 @@
+
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage, AboutPage, ProjectsPage, BlogPage, ContactPage } from './pages';
 import './App.css';
 
+// 1. ScrollToTop bileşenini components klasöründen import ediyoruz
+import { ScrollToTop } from './components/ScrollToTop'; 
+
 function App() {
+  // isDark state'i, sitenin tema durumunu (koyu/açık) tutar
   const [isDark, setIsDark] = useState(true);
 
   return (
     <Router>
+      {/* 2. ScrollToTop bileşenini buraya ekliyoruz */}
+      {/* Bu sayede her sayfa geçişinde scroll en üste atılacak */}
+      <ScrollToTop />
+      
       <Routes>
         <Route path="/" element={<HomePage isDark={isDark} setIsDark={setIsDark} />} />
         <Route path="/hakkimda" element={<AboutPage isDark={isDark} setIsDark={setIsDark} />} />
